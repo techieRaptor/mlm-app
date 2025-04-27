4e8f3661230770e5126f58ae304ef64c9e191be7
Satish <er.satish.cse08@gmail.com>
2025-04-27 22:57:22 +0530

    Initial commit: Spring Boot project setup for MLM App

---
+++ src/main/java/com/mlm/mlm_app/MlmAppApplication.java
@@ -0,0 +1,13 @@
+package com.mlm.mlm_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlmAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlmAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mlm/mlm_app/MlmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlm.mlm_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlmAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
